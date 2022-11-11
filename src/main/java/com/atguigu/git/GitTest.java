6dde12ff78e0ff6795d9419505fd32c0d2e32ae7
yueyue <123456@qq.com>
2022-11-11 17:05:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 31880
+ * @create 2022-11-11 17:03:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
